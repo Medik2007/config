0f841237cbfa678135147bb6094c596df5571658
StevesInTheNet <stevesinthenet@gmail.com>
2024-06-02 13:04:14 +0300

    Backup

--- .config/hypr/scripts/screenshots/screenshot_text.sh
+++ .config/hypr/scripts/screenshots/screenshot_text.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 path="$HOME/stf/img/screenshots/part/$(date +"%S-%M-%H-%d-%m-%Y.png")"
-txt="$HOME/.config/hypr/scripts/screenshot_text"
+txt="$HOME/.config/hypr/scripts/screenshots/screenshot_text"
 grim -g "$(slurp)" $path
 tesseract $path $txt -l eng
 wl-copy "$(cat "${txt}.txt")"

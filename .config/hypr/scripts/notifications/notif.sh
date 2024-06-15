d366e43ba3821a6e9ca4d89ea1a1927e0cdf7693
StevesInTheNet <stevesinthenet@gmail.com>
2024-06-15 13:10:13 +0300

    Backup

--- .config/hypr/scripts/notifications/notif.sh
+++ .config/hypr/scripts/notifications/notif.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+mkdir -p $HOME/.config/hypr/scripts/notifications/notifs/
 notif_id="$HOME/.config/hypr/scripts/notifications/notifs/$2"
 cat $notif_id || echo "0" > $notif_id
 echo $(notify-send -p -u $1 -r $(cat $notif_id) "$3") > $notif_id

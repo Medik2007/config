5ea5fb2a0ceb5e4f860888aa4e9cd81941d034a1
StevesInTheNet <stevesinthenet@gmail.com>
2024-06-19 18:58:37 +0300

    Backup

--- run/update/update.sh
+++ run/update/update.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
+
 neofetch
 yay
 echo
+
+echo Updating system configs
 cd $HOME
 git pull origin master
+echo
+
+echo Updating projects
 cd ~/prj
+
+repo_exclude=('config' 'archive')
+gh_prj=$(($(gh repo list | wc -l) - ${#repo_exclude[@]}))
+local_prj=$(find . -maxdepth 1 -type d ! -name "." | wc -l)
+
+repo_exclude_jq=$(printf '%s\n' "${repo_exclude[@]}" | jq -R . | jq -s .)
+projects=($(gh repo list --json name |
+           jq -r --argjson names "$repo_exclude_jq" '.[] | select(.name as $name | $names | index($name) | not)' |
+           jq -r '.name'))
+
+if [[ $local_prj -lt $gh_prj ]]; then
+    for prj in ${projects[@]}; do
+        if [[ ! -d $prj ]]; then
+            echo
+            echo Installing "$prj" project
+            echo
+            gh repo clone $prj
+        else
+            echo
+            echo Updating "$prj" project
+            echo
+            cd $prj
+            git pull origin master
+            cd ..
+        fi
+    done
+fi
+echo
+echo Full system update complete

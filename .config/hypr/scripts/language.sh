b131551f013f69344806499157a0191929ebb90a
StevesInTheNet <stevesinthenet@gmail.com>
2024-07-14 22:19:07 +0300

    Backup

--- .config/hypr/scripts/language.sh
+++ .config/hypr/scripts/language.sh
@@ -2,12 +2,12 @@
 
 language=$(hyprctl devices -j | jq -r '.keyboards[0].layout')
 
-if [[ $language == "us" ]]; then
-    hyprctl keyword input:kb_layout ru
-    $HOME/.config/hypr/scripts/notifications/notif.sh low language "Switched to Russian"
-else
+if [[ $language == "ru" ]]; then
     hyprctl keyword input:kb_layout us
     $HOME/.config/hypr/scripts/notifications/notif.sh low language "Switched to English"
+else
+    hyprctl keyword input:kb_layout ru
+    $HOME/.config/hypr/scripts/notifications/notif.sh low language "Switched to Russian"
 fi
 
 $HOME/.config/waybar/modules/right/trigger.sh

07ab0cad7091dd982ef11379983c4bd30f5f3f1a
StevesInTheNet <stevesinthenet@gmail.com>
2024-07-21 14:29:04 +0300

    Backup

--- run/utils/act.sh
+++ run/utils/act.sh
@@ -5,13 +5,8 @@ default_PS1() {
 }
 
 act() {
-    (
-        source bin/activate &&
-        export PS1='\n\e[0;35m[$(hostname)] \e[0;37m(\w) \e[0;35m($(basename $VIRTUAL_ENV))\e[0m\n~> '
-    ) || (
-        echo "Could not find virtual env"
-        exit 1
-    )
+    source bin/activate &&
+    export PS1='\n\e[0;35m[$(hostname)] \e[0;37m(\w) \e[0;35m($(basename $VIRTUAL_ENV))\e[0m\n~> '
 }
 
 deact() {

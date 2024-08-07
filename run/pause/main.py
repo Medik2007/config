eefbee580abbeca433bd1a19315d8b673fff3332
StevesInTheNet <stevesinthenet@gmail.com>
2024-08-08 00:37:30 +0300

    Backup

--- run/pause/main.py
+++ run/pause/main.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
 import subprocess
 import json
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def main():
@@ -11,8 +11,16 @@ def main():
 
 @app.route('/pause', methods=['POST'])
 def pause():
-    subprocess.Popen(['wtype', ' '])
-    return json.dumps({'success':True})
+    if request.form['action'] == 'pause':
+        subprocess.Popen(['playerctl', '--all-players', 'pause-play'])
+        return json.dumps({'success':True})
+    elif request.form['action'] == 'translate':
+        subprocess.Popen(['grim', '-g', '320,1 930x900', 'img.png'])
+        subprocess.Popen(['tesseract', 'img.png', 'text', '-l', 'eng'])
+        translation = GoogleTranslator(source='auto', target='ru').translate(subprocess.check_output(['cat', 'text.txt'], text=True))
+        print(translation)
+        return json.dumps({'success':False, 'translation':translation})
+    return json.dumps({'success':False})
 
 
 if __name__ == "__main__":

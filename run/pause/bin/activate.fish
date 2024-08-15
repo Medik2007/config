7fe20c73e376a1f467239345eb1c5077c5df9f0c
StevesInTheNet <stevesinthenet@gmail.com>
2024-08-15 23:58:08 +0300

    Backup

--- run/pause/bin/activate.fish
+++ run/pause/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/medik/run/pause"
+set -gx VIRTUAL_ENV "/home/user/run/pause"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- run/pause/main.py
+++ run/pause/main.py
@@ -5,23 +5,37 @@ import json
 
 app = Flask(__name__)
 
+TRANSLATE_CORDS = [subprocess.check_output(['cat', 'cords0.txt'], text=True), subprocess.check_output(['cat', 'cords1.txt'], text=True)]
+#TRANSLATE_CORDS = ["1729,752 1248x326", "1698,96 1050x337"]
+
+
+def translate_screen(num):
+    cords = TRANSLATE_CORDS[int(num)]
+    subprocess.run(f'grim -g "{cords[:-1]}" img{num}.png', shell=True)
+    subprocess.run(f'tesseract img{num}.png text{num} -l spa', shell=True)
+    text = subprocess.check_output(['cat', f'text{num}.txt'], text=True)
+    if text:
+        return GoogleTranslator(source='auto', target='en').translate(subprocess.check_output(['cat', f'text{num}.txt'], text=True))
+    return ''
+
+
 @app.route('/')
 def main():
     return render_template('main.html')
 
 @app.route('/pause', methods=['POST'])
 def pause():
-    if request.form['action'] == 'pause':
-        subprocess.Popen(['playerctl', '--all-players', 'pause-play'])
+    if request.form['act'] == 'pause':
+        subprocess.run('playerctl --all-players play-pause', shell=True)
         return json.dumps({'success':True})
-    elif request.form['action'] == 'translate':
-        subprocess.Popen(['grim', '-g', '320,1 930x900', 'img.png'])
-        subprocess.Popen(['tesseract', 'img.png', 'text', '-l', 'eng'])
-        translation = GoogleTranslator(source='auto', target='ru').translate(subprocess.check_output(['cat', 'text.txt'], text=True))
+    elif request.form['act'] == 'translate':
+        subprocess.run('playerctl --all-players pause', shell=True)
+        translation = translate_screen('0')
+        if not translation:
+            translation = translate_screen('1')
         print(translation)
-        return json.dumps({'success':False, 'translation':translation})
+        return json.dumps({'success':True, 'translation':translation})
     return json.dumps({'success':False})
 
-
 if __name__ == "__main__":
-    app.run(host='192.168.1.6')
+    app.run(debug=True, host='192.168.1.9')

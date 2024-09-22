c9dfdea6764dfdbdad7787377739f443a06d7b80
StevesInTheNet <stevesinthenet@gmail.com>
2024-09-22 12:46:07 +0300

    Backup

--- run/utils/act.sh
+++ run/utils/act.sh
@@ -5,8 +5,13 @@ default_PS1() {
 }
 
 act() {
-    source bin/activate &&
-    export PS1='\n\e[0;35m[$(hostname)] \e[0;37m(\w) \e[0;35m($(basename $VIRTUAL_ENV))\e[0m\n~> '
+    if [[ $1 ]]; then
+        source ~/prj/.venv/$1/bin/activate &&
+        export PS1='\n\e[0;35m[$(hostname)] \e[0;37m(\w) \e[0;35m($(basename $VIRTUAL_ENV))\e[0m\n~> '
+    else
+        source bin/activate &&
+        export PS1='\n\e[0;35m[$(hostname)] \e[0;37m(\w) \e[0;35m($(basename $VIRTUAL_ENV))\e[0m\n~> '
+    fi
 }
 
 deact() {

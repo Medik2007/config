df72ca707ac2e5dc550f61b2ccd16b2d148c44dc
StevesInTheNet <stevesinthenet@gmail.com>
2024-10-09 13:11:23 +0300

    Backup

--- .config/hypr/scripts/movedesk.sh
+++ .config/hypr/scripts/movedesk.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-
 ws_all=$(hyprctl workspaces -j)
 ws_count=$(echo $ws_all | jq length)
 ws_active=$(hyprctl activeworkspace -j | jq -r '.id')
 current=$ws_active
 success=1
-
 
 while [[ $current != 0 && $current != 11 ]]; do
     if [ $1 == 0 ]; then
@@ -18,7 +16,6 @@ while [[ $current != 0 && $current != 11 ]]; do
     for i in $(seq 0 $((ws_count-1))); do
         id=$(echo $ws_all | jq -r ".[$i]" | jq -r ".id")
         if [ $current == $id ]; then
-            hyprctl dispatch workspace $current
             success=0
             break
         fi
@@ -28,3 +25,14 @@ while [[ $current != 0 && $current != 11 ]]; do
         break
     fi
 done
+
+if [ $success != 0 ]; then
+    if [ $1 == 0 ]; then
+        current=$((ws_active - 1))
+    else
+        current=$((ws_active + 1))
+    fi
+fi
+
+hyprctl dispatch workspace $current
+$HOME/.config/waybar/modules/center/trigger.sh $current

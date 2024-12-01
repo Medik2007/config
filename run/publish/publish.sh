28bb81499121adc9d1a62f77c95f974f92bd5556
StevesInTheNet <stevesinthenet@gmail.com>
2024-12-01 17:42:26 +0300

    Backup

---
+++ run/publish/publish.sh
@@ -0,0 +1,10 @@
+publish() {
+    if [[ $1 == 'port' ]]; then
+        cmd="cd ~/port/public_html && git pull origin master && python manage.py collectstatic --noinput"
+        psd="$HOME/run/publish/portfolio.txt"
+    else
+        echo "Specify which project to publish"
+        return
+    fi
+    sshpass -f $psd ssh -t cz18090@185.114.247.170 $cmd
+}
--- run/utils/run.sh
+++ run/utils/run.sh
@@ -1,5 +1,6 @@
 source ~/run/backup/backup.sh
 source ~/run/update/update.sh
+source ~/run/publish/publish.sh
 
 source ~/run/utils/act.sh
 source ~/run/utils/scad.sh

37d1f1f2cc7e22850a89c7821d4e3eb45d157015
StevesInTheNet <stevesinthenet@gmail.com>
2025-01-01 17:30:49 +0300

    Backup

--- .bashrc
+++ .bashrc
@@ -4,21 +4,19 @@
 
 [[ $- != *i* ]] && return
 
-alias bashrc='source ~/.bashrc'
-alias reflect='sudo reflector --latest 5 --age 2 --fastest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist'
-alias kali='virtualboxvm --startvm Kali & exit'
-alias windows='virtualboxvm --startvm Windows'
-
 alias nv='nvim'
-alias ju='python ~/run/jump/jump.py'
+alias ju='python ~/run/steve/jump.py'
+alias runserver='python manage.py runserver'
+alias bashrc='source ~/.bashrc'
+
 alias ls='ls --color=auto'
 alias l1='ls -1'
 alias ll='ls -goh'
 alias grep='grep --color=auto'
+
 alias backoff='backup && poweroff'
 alias back_pub='backup && printf "\n\n\n" && publish'
 alias bottle='prime-run bottles -b bottle'
-alias mods='cd ~/.local/share/bottles/bottles/fallout/drive_c/users/steamuser/Documents/Paradox\ Interactive/'
 
 source ~/.local/share/blesh/ble.sh
 source ~/run/utils/run.sh
---
+++ run/steve/jump.py
@@ -0,0 +1,73 @@
+import argparse, os, json, sys
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+try:
+    with open(f'{PATH}/data.json', 'r') as file:
+        data = json.load(file)
+except FileNotFoundError:
+    print("Data file not found.")
+    sys.exit(1)
+except json.JSONDecodeError:
+    print("Data file is corrupted.")
+    sys.exit(1)
+
+VENV      = os.path.expanduser(data['venv'])
+JUMP_DIRS = data['jump_dirs']
+SHORTCUTS = data['shortcuts']
+
+
+
+class Jump():
+
+    def act_run(self, dest, command):
+        return f'sh -c "source {VENV}{dest}/bin/activate && {command}; exec $SHELL"'
+
+    def jump(self, args):
+        os.chdir(os.path.expanduser('~'))
+        dest = args.destination
+
+        if dest in SHORTCUTS:
+            os.system(f'sh -c "cd {os.path.expanduser(SHORTCUTS[dest])}; exec $SHELL"')
+            return
+        
+        for dir in JUMP_DIRS:
+            if os.path.isdir(f'{dir}{dest}'):
+                os.chdir(f'{dir}{dest}')
+                if args.jump_only:
+                    os.system(f'sh -c "cd ~/{dir}{dest}; exec $SHELL"')
+                    break
+                if os.path.isdir(VENV + dest):
+                    # DJANGO
+                    if os.path.isfile(f'manage.py'):
+                        if args.server and args.code:
+                            os.system(f'kitty @ launch --type tab --cwd current {self.act_run(dest, "nvim")}')
+                            os.system(self.act_run(dest, 'python manage.py runserver 0.0.0.0:8000'))
+                        elif args.code:
+                            os.system(self.act_run(dest, 'nvim'))
+                        elif args.server:
+                            os.system(self.act_run(dest, 'python manage.py runserver 0.0.0.0:8000'))
+                    # DEFAULT
+                    elif args.code:
+                        os.system(self.act_run(dest, 'nvim'))
+                elif args.code:
+                    os.system('nvim')
+                os.system(f'sh -c "cd ~/{dir}{dest}; exec $SHELL"')
+                break
+        else:
+            print('Jump destination not found')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Jumping program")
+
+    parser.add_argument("destination", help="Jump destination")
+    parser.add_argument('-s', '--server', action='store_false', help="Don't start django server")
+    parser.add_argument('-c', '--code',   action='store_false', help="Don't open code")
+    parser.add_argument('-j', '--jump-only', action='store_true', help="Don't start any programs, only cd")
+
+    args = parser.parse_args()
+
+    jump = Jump()
+    jump.jump(args)

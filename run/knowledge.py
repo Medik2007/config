6406ef6c46725ca2bd2db0395d5b1b72f0b406c9
StevesInTheNet <stevesinthenet@gmail.com>
2025-01-01 23:41:00 +0300

    Backup

--- run/knowledge.py
+++ run/knowledge.py
@@ -1,4 +1,4 @@
-import os, json
+import os, argparse, json
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
@@ -9,7 +9,19 @@ class Knowledge():
         title = input()
         command = input()
 
+        with open(f'{PATH}/data/jump.json', 'r') as file:
+            data = json.load(file)
+
+        data['commands'][title] = command
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Jumping program")
+    parser.add_argument('file', nargs='?', default='default.txt', help="File to process (default: default.txt)")
+    args = parser.parse_args()
+
     knowledge = Knowledge()
     knowledge.main()

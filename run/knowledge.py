5e101a3407f2b5444f983a0cb1dcee5e055fc660
StevesInTheNet <stevesinthenet@gmail.com>
2025-01-01 21:58:58 +0300

    Backup

---
+++ run/knowledge.py
@@ -0,0 +1,15 @@
+import os, json
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+class Knowledge():
+
+    def main(self):
+        title = input()
+        command = input()
+
+
+if __name__ == '__main__':
+    knowledge = Knowledge()
+    knowledge.main()

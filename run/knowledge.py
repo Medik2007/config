a802b05fde982d937542a5e36937a436b6f78678
StevesInTheNet <stevesinthenet@gmail.com>
2025-01-03 17:28:24 +0300

    Backup

--- run/knowledge.py
+++ run/knowledge.py
@@ -3,7 +3,64 @@ import os, argparse, json, sys, tty, termios
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 
+
+
+class Editor():
+    def get_key(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)  # Set terminal to raw mode
+            key = sys.stdin.read(1)  # Read a single character
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)  # Restore settings
+        return key
+
+    def render(self, lines, cursor_row):
+        sys.stdout.write("\033[H\033[J")  # Clear the terminal]]
+        for i, line in enumerate(lines):
+            # Highlight the line with the cursor
+            if i == cursor_row:
+                sys.stdout.write(f"> {line}\n")
+            else:
+                sys.stdout.write(f"  {line}\n")
+
+    def main(self, lines):
+        cursor_row = 0
+        while True:
+            self.render(lines, cursor_row)
+            key = self.get_key()
+
+            if key == "\r": # Enter
+                cursor_row += 1
+                lines.insert(cursor_row, "")
+
+            elif key == "\x7f": # Backspace
+                if lines[cursor_row]:
+                    lines[cursor_row] = lines[cursor_row][:-1]
+                elif cursor_row > 0:
+                    lines.pop(cursor_row)
+                    cursor_row -= 1
+
+            elif key == "\033":  # Arrow keys
+                next_key = self.get_key()
+                if next_key == "[": # ]
+                    direction = self.get_key()
+                    if direction == "A" and cursor_row > 0: # Up
+                        cursor_row -= 1
+                    elif direction == "B" and cursor_row < len(lines) - 1: # Down
+                        cursor_row += 1
+
+            elif key == "\x03": # ^C
+                 break
+
+            else:
+                lines[cursor_row] += key
+
+
 class Knowledge():
+    def __init__(self):
+        self.editor = Editor()
     def read_data(self):
         with open(f'{PATH}/data/knowledge.json', 'r') as file:
             return json.load(file)
@@ -20,61 +77,13 @@ class Knowledge():
             except ValueError:
                 print('Input a number')
         return response
-    def get_key(self):
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)  # Set terminal to raw mode
-            key = sys.stdin.read(1)  # Read a single character
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)  # Restore settings
-        return key
-    def render_text(self, lines, cursor_row):
-        sys.stdout.write("\033[H\033[J")  # Clear the terminal]]
-        for i, line in enumerate(lines):
-            # Highlight the line with the cursor
-            if i == cursor_row:
-                sys.stdout.write(f"> {line}\n")
-            else:
-                sys.stdout.write(f"  {line}\n")
 
 
-    def write(self):
+    def open_record(self):
         #title = input()
         #print('~'*len(title) + '\n')
         lines = [""]
-        cursor_row = 0
-        try:
-            while True:
-                self.render_text(lines, cursor_row)
-                key = self.get_key()
-
-                if key == "\r":  # Enter key
-                    cursor_row += 1
-                    lines.insert(cursor_row, "")
-                elif key == "\x7f":  # Backspace key
-                    if lines[cursor_row]:
-                        # Delete the last character in the current line
-                        lines[cursor_row] = lines[cursor_row][:-1]
-                    elif cursor_row > 0:
-                        # If the line is empty, delete the line and move the cursor up
-                        lines.pop(cursor_row)
-                        cursor_row -= 1
-                elif key == "\033":  # Arrow keys (start with ESC)
-                    next_key = self.get_key()
-                    if next_key == "[":#]
-                        direction = self.get_key()
-                        if direction == "A" and cursor_row > 0:  # Up arrow
-                            cursor_row -= 1
-                        elif direction == "B" and cursor_row < len(lines) - 1:  # Down arrow
-                            cursor_row += 1
-                elif key == "\x03":
-                     break
-                else:
-                    # Append the typed character to the current line
-                    lines[cursor_row] += key
-        except KeyboardInterrupt:
-            pass
+        self.editor.main(lines)
 
         #data = self.read_data()
         #data[title] = text
@@ -117,7 +126,7 @@ class Knowledge():
         if args.query:
             self.search(args.query)
         else:
-            self.write()
+            self.open_record()
 
 
 if __name__ == '__main__':
